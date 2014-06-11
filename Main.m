259b2521b7ef9f32e109b54c6bd517f4a4cfabcd
Qmegas <qmegas@gmail.com>
2014-06-11 16:01:21 +0300

    Updating main

--- Main.m
+++ Main.m
@@ -9,6 +9,9 @@ function [ output_args ] = Main( )
     %Step 3 - Split to components
     Frames = Step3_Componenting(I);
     
+    %Step 4 - Merging components
+    Frames = Step4_ComponentMerging(Frames);
+    
     %Debug
     imshow(I);
     for k = 1:length(Frames)
@@ -17,22 +20,22 @@ function [ output_args ] = Main( )
     
     return;
     
-    %Step 4 - Merging components
-    Frames = Step4_ComponentMerging(Frames);
-    
     %Step 5 - Creating vector
     letter_vectors = cell(length(Frames), 1);
     for k = 1:length(Frames)
         frame_data = Frames{k};
         letter_matrix = I(frame_data(2):(frame_data(2)+frame_data(4)), frame_data(1):(frame_data(1)+frame_data(3)));
+        imshow(letter_matrix);
+        pause(3);
         letter_vectors{k} = Step5_Letter2Vector(letter_matrix);
     end
     
     %Step 6 - Running DTW for each component
     for k = 1:length(Frames)
         comparison_table = Step6_DTW(letter_vectors{k});
-        %TODO: Step 6.5 - Find bigest value + output letter
     end
-
+    
+    %Step 6.5 - Find bigest value + output letter
+    %TODO
 end
 
