f299c0c9ea60384a1d4d14f13c58243ca4df847e
Qmegas <qmegas@gmail.com>
2014-06-15 18:05:25 +0300

    Update Step6_DTW.m

--- Step6_DTW.m
+++ Step6_DTW.m
@@ -1,7 +1,19 @@
-function [ output_args ] = Step6_DTW( input_args )
-%STEP6_DTW Summary of this function goes here
-%   Detailed explanation goes here
-
-
+function [ DistsShapeContext ] = Step6_DTW( letter_vector, DB_letters )
+    NumOfSamples = size(DB_letters, 1);
+    
+    [TestSeqAsShapeContext] = Lib_MakeShapeShapeContextFeature(letter_vector);
+    
+    DistsShapeContext = zeros(NumOfSamples, 1);
+    k = 1;
+    
+    for j = 1:NumOfSamples
+        NextLetSeq = DB_letters{j};
+        
+        [NextLetSeqAsShapeContext] = Lib_MakeShapeShapeContextFeature(NextLetSeq);
+        
+        % DTW Distance using P = 2
+        [Diff,~,~,~,~] = Lib_DTW_CalcDistance(TestSeqAsShapeContext, NextLetSeqAsShapeContext);
+        DistsShapeContext(k) = Diff;
+        k = k + 1;
+    end
 end
-
