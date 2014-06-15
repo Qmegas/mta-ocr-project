9c029f7aeca5bb83af370671468afb215ea4cd08
Qmegas <qmegas@gmail.com>
2014-06-15 17:03:19 +0300

    Implementation of step 5

--- Step5_Letter2Vector.m
+++ Step5_Letter2Vector.m
@@ -1,4 +1,17 @@
 function [ letter_vector ] = Step5_Letter2Vector( letter_matrix )
-    %TODO
+    sizes = size(letter_matrix);
+    final_vector = zeros(sum(sum(letter_matrix > 0)), 2);
+    vector_size = 0;
+    
+    for row = 1:sizes(1)
+        for col = 1:sizes(2)
+            if letter_matrix(row, col) > 0
+                vector_size = vector_size + 1;
+                final_vector(vector_size, 1) = row;
+                final_vector(vector_size, 2) = col;
+            end
+        end
+    end
+    
+    letter_vector = final_vector;
 end
-
